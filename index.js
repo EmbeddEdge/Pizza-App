81581a4f2e564906b8506fe5a8005eed9dd31da9
EmbeddEdge <turynlb@gmail.com>
2025-07-04 23:44:40 +0200

    Added first code samples

---
+++ index.js
@@ -0,0 +1,38 @@
+const menu = [
+    {name: 'Margerita', price: 8 },
+    {name: 'Pepperoni', price: 9 },
+    {name: 'Hawaiian', price: 10 },
+    {name: 'Veggie', price: 7 },
+]
+
+const cashInRegister = 100
+const orderQueue = []
+
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj)
+}
+
+function placeOrder(pizzaName) {
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === PizzaName)
+    cashInRegister += selectedPizza.price
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    orderQueue.push(newOrder)
+    return newOrder
+}
+
+function completeOrder(orderId) {
+    const order = orderQueue.find(order => order.id === orderId)
+    order.status = "completed"
+    return order
+}
+
+addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 });
+addNewPizza({ name: "BBQ Chicken", cost: 12 });
+addNewPizza({ name: "Spicy Sausage", cost: 12 });
+
+placeOrder("Chicken Bacon Ranch")
+completeOrder("1")
+
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
